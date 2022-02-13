9c005be7a0d7661456a2e4753597478f8ef7ebf3
Patrick Bruce <34798783+pnbruce@users.noreply.github.com>
2022-02-13 10:03:56 -0600

    saves the board state even if you leave the page

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,5 +1,11 @@
 let xToMove = true
-let boardState = [0,0,0,0,0,0,0,0,0]
+let boardState
+if(localStorage.getItem('boardState')) {
+    boardState = JSON.parse(localStorage.getItem('boardState'))
+} else {
+    boardState = [0,0,0,0,0,0,0,0,0]
+}
+
 let board = [
     document.getElementById('1,1'), document.getElementById('1,2'), document.getElementById('1,3'),
     document.getElementById('2,1'), document.getElementById('2,2'), document.getElementById('2,3'),
@@ -10,13 +16,23 @@ let boardListeners = [null, null, null, null, null, null, null, null, null,]
 updateBoard(xToMove, boardState, board, boardListeners)
 
 function updateBoard(xToMove, boardState, board, boardListeners) {
+    // update every position on the board to reflect board state
+    for (let i = 0; i < 9; i++) {
+        board[i].textContent = '  ' + (!boardState[i] ? ' ' : (boardState[i] == 1 ? 'X' : 'O'))  + '  '
+    }
+    //determine if someone won and who it was
     let winner = whoWon(boardState)
+    // stop the game and add text inicating the winner
     if ( !!winner == true) {
-        document.getElementById('title').textContent = (winner == 1 ? 'X' : 'O') + " wins!"
+        document.getElementById('title').textContent = winner == 2 ? 'Cats game!' : (winner == 1 ? 'X' : ('O')) + " wins!"
         for (let i = 0; i < 9; i++) {
             board[i].removeEventListener('click', boardListeners[i])
         }
-    } else {
+        localStorage.removeItem('boardState')
+    }
+    // if there is no winner indicate who the next person to move is 
+    else {
+        document.getElementById('title').textContent = (xToMove ? 'X' : 'O') + ' to move'
         for (let i = 0; i < 9; i++) {
             if(boardState[i] === 0) {
                 if(boardListeners[i] !== null) {
@@ -34,16 +50,12 @@ function updateBoard(xToMove, boardState, board, boardListeners) {
 function makePostionListener(i, xToMove, boardState, board, boardListeners) {
     function postionOnClick() {
         if(xToMove === true){
-            board[i].textContent = '  X  '
-
             boardState[i] = 1
         } else {
-            board[i].textContent = '  O  '
-            
             boardState[i] = -1
         }
+        localStorage.setItem('boardState', JSON.stringify(boardState))
         xToMove = !xToMove
-        document.getElementById('title').textContent = (xToMove ? 'X' : 'O') + ' to move'
         updateBoard(xToMove, boardState, board, boardListeners)
     }
     return postionOnClick
@@ -74,7 +86,11 @@ function whoWon(boardState) {
     if(threeOfAKind(boardState[2], boardState[4], boardState[6])) {
         return boardState[2]
     }
-    return 0;
+    if(boardState[0] && boardState[1] && boardState[2] && boardState[3] && boardState[4] && boardState[5] && boardState[6] && boardState[7] && boardState[8])
+    {
+        return 2
+    }
+    return 0
 }
 
 function threeOfAKind(a, b, c) {
