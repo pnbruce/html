bab3211e4126be15c41e4f477afc811fdab43d4b
Patrick Bruce <34798783+pnbruce@users.noreply.github.com>
2022-02-12 22:30:12 -0600

    can no longer overwrite the other players moves

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,25 +1,41 @@
-xToMove = true
-
+let xToMove = true
+let boardState = [0,0,0,0,0,0,0,0,0]
 let board = [
-    [document.getElementById('1,1'), document.getElementById('1,2'), document.getElementById('1,3')],
-    [document.getElementById('2,1'), document.getElementById('2,2'), document.getElementById('2,3')],
-    [document.getElementById('3,1'), document.getElementById('3,2'), document.getElementById('3,3')]
+    document.getElementById('1,1'), document.getElementById('1,2'), document.getElementById('1,3'),
+    document.getElementById('2,1'), document.getElementById('2,2'), document.getElementById('2,3'),
+    document.getElementById('3,1'), document.getElementById('3,2'), document.getElementById('3,3')
 ]
+let boardListeners = [null, null, null, null, null, null, null, null, null,]
 
-board.forEach((element) => {
-    element.forEach((element) => {
-        //if(element.textContent === '     ') {
-            element.addEventListener('click', function() {
-                if(xToMove === true){
-                    element.textContent = '  X  '
-                    document.getElementById('title').textContent = 'O to move'
-                } else {
-                    element.textContent = '  O  '
-                    document.getElementById('title').textContent = 'X to move'
-                }
-                xToMove = !xToMove
-                checkForWin(board);
-            })
-        //}
-    })
-})
+updateBoard(xToMove, boardState, board, boardListeners)
+
+function updateBoard(xToMove, boardState, board, boardListeners) {
+    for (let i = 0; i < 9; i++) {
+        if(boardState[i] === 0) {
+            if(boardListeners[i] !== null) {
+                board[i].removeEventListener('click', boardListeners[i])
+            }
+            boardListeners[i] = makePostionListener(i, xToMove, boardState, board, boardListeners)
+            board[i].addEventListener('click', boardListeners[i])
+        } else {
+            board[i].removeEventListener('click', boardListeners[i])
+        }
+    }
+}
+
+function makePostionListener(i, xToMove, boardState, board, boardListeners) {
+    function postionOnClick() {
+        if(xToMove === true){
+            board[i].textContent = '  X  '
+            document.getElementById('title').textContent = 'O to move'
+            boardState[i] = 1
+        } else {
+            board[i].textContent = '  O  '
+            document.getElementById('title').textContent = 'X to move'
+            boardState[i] = -1
+        }
+        xToMove = !xToMove
+        updateBoard(xToMove, boardState, board, boardListeners)
+    }
+    return postionOnClick
+}
